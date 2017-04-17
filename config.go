f6c604086f5b9e366609312c91c887e365ed6b2e
laixingchen <laixingchen01@163.com>
2017-04-17 15:56:33 +0800

    change to 'toml'

--- config.go
+++ config.go
@@ -1,151 +1,69 @@
 package config
 
 import (
-	"bufio"
-	"encoding/json"
-	"os"
-	"strconv"
-	"strings"
+	"github.com/pelletier/go-toml"
+	"time"
 )
 
-var mConfig map[string]string
+var c_path = "config.toml"
 
-const DEFAULT_CONFIG = "default.config"
+var c *toml.TomlTree
+
+func SetPath(p string) {
+	c_path = p
+}
+
+func ReLoad() error {
+	var err error
+	c, err = toml.LoadFile(c_path)
+	return err
+}
 
 func init() {
-	if mConfig == nil {
-		mConfig = make(map[string]string)
-		load(DEFAULT_CONFIG)
-	}
+	ReLoad()
 }
 
-func Reload(p string) {
-	mConfig = make(map[string]string)
-	load(p)
-}
-
-func load(p string) {
-	f, e := os.Open(p)
-	if e != nil {
-		panic(e)
-	}
-	defer f.Close()
-	r := bufio.NewReader(f)
-	var err error = nil
-	var line []byte
-	for err == nil {
-		line, _, err = r.ReadLine() //逐行读取
-		s := strings.TrimSpace(string(line))
-		if len(s) == 0 { //跳过空行
-			continue
-		}
-		if strings.HasPrefix(s, "//") {
-			continue
-		}
-		i := strings.Index(s, "=")
-		commentIndex := strings.Index(s, "//")
-		if commentIndex < 0 {
-			commentIndex = len(s)
-		}
-		if i > 0 {
-			key := s[:i]
-			v := s[i+1 : commentIndex]
-			mConfig[strings.TrimSpace(key)] = strings.TrimSpace(v)
-		}
-	}
-}
-
-func String(key string, def ...interface{}) string {
-	v := mConfig[key]
-	if v == "" && len(def) > 0 {
-		v, _ = def[0].(string)
+func String(key string, def ...string) string {
+	v, ok := c.Get(key).(string)
+	if !ok && len(def) > 0 {
+		return def[0]
 	}
 	return v
 }
 
-func Int(key string, def ...interface{}) int {
-	s := String(key)
-	var v int
-	if s == "" && len(def) > 0 {
-		v, _ = def[0].(int)
-		return v
+func Int64(key string, def ...int64) int64 {
+	v, ok := c.Get(key).(int64)
+	if !ok && len(def) > 0 {
+		return def[0]
 	}
-	v, _ = strconv.Atoi(s)
 	return v
 }
 
-func Int64(key string, def ...interface{}) int64 {
-	s := String(key)
-	var v int64
-	if s == "" && len(def) > 0 {
-		v, _ = def[0].(int64)
-		return v
+func Float64(key string, def ...float64) float64 {
+	v, ok := c.Get(key).(float64)
+	if !ok && len(def) > 0 {
+		return def[0]
 	}
-	v, _ = strconv.ParseInt(s, 10, 64) //10进制 64位
 	return v
 }
 
-func Int32(key string, def ...interface{}) int32 {
-	s := String(key)
-	var v int32
-	if s == "" && len(def) > 0 {
-		v, _ = def[0].(int32)
-		return v
+func Bool(key string, def ...bool) bool {
+	v, ok := c.Get(key).(bool)
+	if !ok && len(def) > 0 {
+		return def[0]
 	}
-	tmp, _ := strconv.ParseInt(s, 10, 32) //10进制 32位
-	return int32(tmp)
-}
-
-func Int8(key string, def ...interface{}) int8 {
-	s := String(key)
-	var v int8
-	if s == "" && len(def) > 0 {
-		v, _ = def[0].(int8)
-		return v
-	}
-	tmp, _ := strconv.ParseInt(s, 10, 8) //10进制 8位
-	return int8(tmp)
-}
-
-func Float64(key string, def ...interface{}) float64 {
-	s := String(key)
-	var v float64
-	if s == "" && len(def) > 0 {
-		v, _ = def[0].(float64)
-		return v
-	}
-	v, _ = strconv.ParseFloat(s, 64)
-	return v
-}
-func Float32(key string, def ...interface{}) float32 {
-	s := String(key)
-	var v float32
-	if s == "" && len(def) > 0 {
-		v, _ = def[0].(float32)
-		return v
-	}
-	tmp, _ := strconv.ParseFloat(s, 64)
-	return float32(tmp)
-}
-
-func Float(key string, def ...interface{}) float64 {
-	return Float64(key, def...)
-}
-
-func Bool(key string, def ...interface{}) bool {
-	s := String(key)
-	var v bool
-	if s == "" && len(def) > 0 {
-		v, _ = def[0].(bool)
-		return v
-	}
-	v, _ = strconv.ParseBool(s)
 	return v
 }
 
-func StringArray(key string) []string {
-	s := String(key)
-	var v []string
-	json.Unmarshal([]byte(s), &v)
+func Time(key string, def ...time.Time) time.Time {
+	v, ok := c.Get(key).(time.Time)
+	if !ok && len(def) > 0 {
+		return def[0]
+	}
+	return v
+}
+
+func Array(key string) []interface{} {
+	v, _ := c.Get(key).([]interface{})
 	return v
 }
--- config_test.go
+++ config_test.go
@@ -4,31 +4,17 @@ import (
 	"testing"
 )
 
-func Test_String(t *testing.T) {
-	t.Log(String("key"))
-	t.Log(String("foo"))
+func TestConfig(t *testing.T) {
 	t.Log(String("string"))
-	t.Log(String("not_exist_key","default value"))
-}
-
-func Test_Int(t *testing.T) {
-	t.Log(Int("int"))
-	t.Log(Int32("int"))
 	t.Log(Int64("int"))
-	t.Log(Int("bogus_int"))
-}
-
-func Test_Float(t *testing.T){
-	t.Log(String("float"))
-	t.Log(Float("float"))
 	t.Log(Float64("float"))
-	t.Log(Float32("float"))
-	t.Log(Float("foo"))
-}
-
-func Test_Bool(t *testing.T) {
 	t.Log(Bool("bool"))
-	t.Log(Bool("bool2"))
-	t.Log(Bool("bool3"))
-	t.Log(Bool("string"))
+	t.Log(Array("array1"))
+	t.Log(Array("array2"))
+
+	//toml table
+	t.Log(String("foo.a"))
+	t.Log(Int64("foo.b"))
+	t.Log(Float64("foo.c"))
+	t.Log(Array("foo.d"))
 }
